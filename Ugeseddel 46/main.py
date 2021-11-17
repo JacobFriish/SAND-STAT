246215b623d32df557f4ee240c223fd52a72ec3e
JacobFriish <93619265+JacobFriish@users.noreply.github.com>
2021-11-17 16:29:08 +0100

    Update main.py

--- Ugeseddel 46/main.py
+++ Ugeseddel 46/main.py
@@ -1,25 +1,39 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import pandas as pd
 import statistics
 from scipy.stats import kurtosis, skew, describe
 
 
 data = pd.read_csv("data uge 46.csv")
-print(data)
-
 
 print("\n")
 print("Opgave 3.1")
+# for at se data anvend:
+# print(data)
 print("Data er diskrete.")
 
 print("\n")
 print("Opgave 3.2")
 
+# Vælg hvilke kolonner der skal plottes
+studietimer = data['studietimer']
+ids = data['id']
+# Vælg hvordan data skal inddeles i bins
+
 # Settings til histogrammet
-plt.style.use('fivethirtyeight')
 plt.title('Histogram, karakterer og studietimer')
 plt.xlabel('Studietimer')
-plt.ylabel('Density')
+plt.ylabel('Antal')
 plt.tight_layout()
+
+# Plot median-linje
+median_studietimer = statistics.median(studietimer)
+plt.axvline(median_studietimer, color='red', label='Median')
+
+# Plot selve grafen
+plt.hist(studietimer, histtype='bar', edgecolor='black')
 plt.show()
+
+
+
